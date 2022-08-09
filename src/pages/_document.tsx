3e79fd62d5bdac1c76cb48be007b064802e0d815
erick-menezes <erickmenezes25@gmail.com>
2022-08-09 19:54:38 -0300

    Changing method of font import to make compatible with build version

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <meta charSet="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+        <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&display=swap" rel="stylesheet"></link>
         <meta name="description" content="Full Stack Developer Portfolio made by Erick Menezes" />
       </Head>
       <body>
